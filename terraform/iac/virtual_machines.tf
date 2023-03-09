11706b30f343b60955eb80a752511a619ad38531
Matcham89-AB <chris.matcham@appsbroker.com>
2023-03-09 14:03:29 +0000

    updates to nic

--- terraform/iac/virtual_machines.tf
+++ terraform/iac/virtual_machines.tf
@@ -15,8 +15,8 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "v_network_int" {
-  name                = "ramsay-nic"
+resource "azurerm_network_interface" "v_network_int_1" {
+  name                = "ramsay-nic-1"
   location            = local.resource_location
   resource_group_name = var.resource_group
 
@@ -27,6 +27,17 @@ resource "azurerm_network_interface" "v_network_int" {
   }
 }
 
+resource "azurerm_network_interface" "v_network_int_2" {
+  name                = "ramsay-nic-2"
+  location            = local.resource_location
+  resource_group_name = var.resource_group
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 #
 # virtual machine
 #
@@ -40,7 +51,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm_1" {
   admin_password                  = azurerm_key_vault_secret.linux_vm_1_secret.value
   disable_password_authentication = false
   network_interface_ids = [
-    azurerm_network_interface.v_network_int.id,
+    azurerm_network_interface.v_network_int_1.id,
   ]
 
   os_disk {
@@ -69,7 +80,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm_2" {
   admin_password                  = azurerm_key_vault_secret.linux_vm_1_secret.value
   disable_password_authentication = false
   network_interface_ids = [
-    azurerm_network_interface.v_network_int.id,
+    azurerm_network_interface.v_network_int_2.id,
   ]
 
 
